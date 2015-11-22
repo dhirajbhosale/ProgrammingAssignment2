4fef3b3db5f22b0af9191a73572c74ddc0dd0a54
dhirajbhosale <dhirajbhosale82@yahoo.com>
2015-11-22 23:17:41 +0530

    Added a function code to file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Using this, the following allows caching the
+# inverse of the matrix and returning it from
+# cached value rather than computing it again
 
-## Write a short comment describing this function
+#----------------------------------------------------
+# Creates a special matrix type from a given matrix x
+# with handle functions get, set, getinv, setinv that
+# encapsulates the access to the underlying matrix and
+# the cached inverse matrix.
+#----------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		# When called, empty cache containing inverse:
+		    invm <- NULL
+		    # Sets the matrix to the given value and cleans cache:
+		    set <- function(y) {
+			x <<- y
+			invm <<- NULL
+		    }
+		    # Returns the Matrix:
+		    get <- function() x
+		    # Sets the cached invm value to the given value
+		    setinv <- function(inverse) invm <<- inverse
+		    # Returns the cached value:
+		    getinv <- function() invm
+		    # List of the handle functions:
+		    list(set = set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+#------------------------------------------------------------
+# Returns the inverse (invm) of a special "matrix" type (x)
+# created by makeCacheMatrix either from the cached value 
+# or from a computed value if the cach is empty (invm = NULL) 
+#-------------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Attempt to get inverse from cached value:
+        invm <- x$getinv()
+        if (!is.null(invm)) {
+            message("getting cached data")
+            return(invm)
+        }
+        # If cached value is empty (NULL) compute the inverse:
+        mtrx <- x$get()
+        message("Computing inverse, this can take a while...")
+        invm <- solve(mtrx,...)
+        # Set the cache to the current value
+        x$setinv(invm)
+        invm        
 }
